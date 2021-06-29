112fb3c09ab58d5e1b65f39ee559de5544ef31d5
Mahmouud74 <64709199+Mahmouud74@users.noreply.github.com>
2021-06-29 18:02:53 +0200

    Update assesment.api.js

--- api/assesment.api.js
+++ api/assesment.api.js
@@ -37,8 +37,7 @@ assesment.get("/solveAssesment/:assesmentId", async(req,res)=>{
     const assesment = await assesmentModel.findOne({_id })
     res.json({assesment});         
      
-})
-assesment.delete("/deleteAssesment/:assesmentId",async(req,res)=>{
+})assesment.delete("/deleteAssesmet/:assesmentId",async(req,res)=>{
     const {username , token} = req.body;
     jwt.verify(token , "instructor" , async(err,decodded)=>{
         if(err){
@@ -48,15 +47,22 @@ assesment.delete("/deleteAssesment/:assesmentId",async(req,res)=>{
             let _id = req.params.assesmentId;
             const assesment = await assesmentModel.findOne({_id })
             const instructor = await userModel.findOne({username});
-            const course = await courseModel.findOne({_id:assesment.courseId , instructorId:instructor._id});
-            if(course){
-                await assesmentModel.deleteOne({_id});
-                res.json({message:`assesmt ${assesment.title} deleted`});
+            if(instructor){
+                const course = await courseModel.findOne({_id:assesment.courseId , instructorId:instructor._id});
+                if(course){
+                    await assesmentModel.deleteOne({_id});
+                    res.json({message:`assesmt ${assesment.title} deleted`});
+                }
+                else{
+                    res.json({message:"you haven't acces to this exam"})
+                }
             }
             else{
-                res.json({message:"you haven't acces to this exam"})
+                res.json({message:"invalid username"})
             }
+
         }
     })
 })
+
 module.exports=assesment;
