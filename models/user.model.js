5fc5548962c64787a5ad0d40f8d4c58c09ad071b
Mahmouud74 <64709199+Mahmouud74@users.noreply.github.com>
2021-06-30 16:18:30 +0200

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -14,6 +14,7 @@ const { db } = require('./grade.model');
    ],
    role: { type: String, default: 'student', enum: ["student", "instructor", "admin"] , required:true },
    imageUrl:{type:String},
+   mobilePhone:{type:String},
 
    //accessToken: { type: String },
    //dateOfJoin: { type: String },
