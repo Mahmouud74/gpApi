f603d5816c719b444be4b3e4d6dcb26f9e4f8369
Mahmouud74 <64709199+Mahmouud74@users.noreply.github.com>
2021-07-01 16:34:21 +0200

    Update userProfile.api.js

--- api/userProfile.api.js
+++ api/userProfile.api.js
@@ -32,4 +32,31 @@ profile.get('/userProfile/:username',async(req,res)=>{
     }
 })
 
+profile.get('/userprofile/:id',async(req,res)=>{
+    let userID = req.params.id;
+    let user = await userModel.findOne({_id:userID});
+    console.log(user.role);
+    if(user.role === "student")
+    {
+        let studentCourses = await studentCourseModel.find({userID})
+        let registeredCourses=[] ;
+        for(let i=0;i<studentCourses.length;i++){ 
+              registeredCourses.push(await courseModel.findOne({_id:studentCourses[i].courseId}))
+        }
+        let assigmentSolutions = await assigmentSolutionModel.find({userId:user._id})
+        res.json({user,registeredCourses , assigmentSolutions})
+    }
+    else if (user.role === "instructor"){
+        let instructorCourses = await courseModel.find({instructorId: user._id })
+        res.json({user , instructorCourses});
+    }
+    else if (user.role === "admin"){
+        res.json({user});
+    }
+    else
+    {
+        res.send('hello')
+
+    }
+})
 module.exports=profile;
