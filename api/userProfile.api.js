cd320ca1cba6cb89c4d47be672cf3b6a7137b281
Mahmouud74 <64709199+Mahmouud74@users.noreply.github.com>
2021-07-02 14:12:30 +0200

    Update userProfile.api.js

--- api/userProfile.api.js
+++ api/userProfile.api.js
@@ -7,56 +7,69 @@ const assigmentSolutionModel = require('../models/assigmentSolution.model')
 profile.get('/userProfile/:username',async(req,res)=>{
     let username = req.params.username;
     let user = await userModel.findOne({username});
-    console.log(user.role);
-    if(user.role === "student")
-    {
-        let studentCourses = await studentCourseModel.find({userID:user._id})
-        let registeredCourses=[] ;
-        for(let i=0;i<studentCourses.length;i++){ 
-              registeredCourses.push(await courseModel.findOne({_id:studentCourses[i].courseId}))
+    if(user){
+        console.log(user.role);
+        if(user.role === "student")
+        {
+            let studentCourses = await studentCourseModel.find({userID:user._id})
+            let registeredCourses=[] ;
+            for(let i=0;i<studentCourses.length;i++){ 
+                  registeredCourses.push(await courseModel.findOne({_id:studentCourses[i].courseId}))
+            }
+            let assigmentSolutions = await assigmentSolutionModel.find({userId:user._id})
+            res.json({user,registeredCourses , assigmentSolutions})
         }
-        let assigmentSolutions = await assigmentSolutionModel.find({userId:user._id})
-        res.json({user,registeredCourses , assigmentSolutions})
+        else if (user.role === "instructor"){
+            let instructorCourses = await courseModel.find({instructorId: user._id })
+            res.json({user , instructorCourses});
+        }
+        else if (user.role === "admin"){
+            res.json({user});
+        }
+        else
+        {
+            res.send('hello')
+    
+        }
     }
-    else if (user.role === "instructor"){
-        let instructorCourses = await courseModel.find({instructorId: user._id })
-        res.json({user , instructorCourses});
+    else{
+        res.json({message:"incorrect username"})
     }
-    else if (user.role === "admin"){
-        res.json({user});
-    }
-    else
-    {
-        res.send('hello')
 
+})
+profile.get('/profile/:userId',async(req,res)=>{
+    let _id = req.params.userId;
+    let user = await userModel.findOne({_id});
+    if(user){
+        console.log(user);
+        // console.log(user.role);
+         if(user.role === "student")
+         {
+             let studentCourses = await studentCourseModel.find({userID:user._id})
+             let registeredCourses=[] ;
+             for(let i=0;i<studentCourses.length;i++){ 
+                   registeredCourses.push(await courseModel.findOne({_id:studentCourses[i].courseId}))
+             }
+             let assigmentSolutions = await assigmentSolutionModel.find({userId:user._id})
+             res.json({user,registeredCourses , assigmentSolutions})
+         }
+         else if (user.role === "instructor"){
+             let instructorCourses = await courseModel.find({instructorId: user._id })
+             res.json({user , instructorCourses});
+         }
+         else if (user.role === "admin"){
+             res.json({user});
+         }
+         else
+         {
+             res.send('hello')
+     
+         }
     }
+    else{
+        res.json({message:"incorrect userId"})
+    }
+
 })
 
-profile.get('/userprofile/:userId',async(req,res)=>{
-    let userID = req.params.userId;
-    let user = await userModel.findOne({_id:userID});
-    console.log(user.role);
-    if(user.role === "student")
-    {
-        let studentCourses = await studentCourseModel.find({userID:user._id})
-        let registeredCourses=[] ;
-        for(let i=0;i<studentCourses.length;i++){ 
-              registeredCourses.push(await courseModel.findOne({_id:studentCourses[i].courseId}))
-        }
-        let assigmentSolutions = await assigmentSolutionModel.find({userId:user._id})
-        res.json({user,registeredCourses , assigmentSolutions})
-    }
-    else if (user.role === "instructor"){
-        let instructorCourses = await courseModel.find({instructorId: user._id })
-        res.json({user , instructorCourses});
-    }
-    else if (user.role === "admin"){
-        res.json({user});
-    }
-    else
-    {
-        res.send('hello')
-
-    }
-})
 module.exports=profile;
